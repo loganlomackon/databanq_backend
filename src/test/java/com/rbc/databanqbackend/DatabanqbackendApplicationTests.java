68b187772da7ee2046572e8e13a9f00ac26fcf77
loganlomackon <breezemelodymackon@gmail.com>
2020-08-19 13:33:09 +0800

    Project init

---
+++ src/test/java/com/rbc/databanqbackend/DatabanqbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbc.databanqbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabanqbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
